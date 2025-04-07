8ffdd97f7cc21f03ac5bebe855e98a446debc672
Florencia Salgado <florikodsgn@gmail.com>
2025-04-07 08:37:30 -0300

    Ajuste rutas 3

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,6 +1,6 @@
-import Header from "./Header/Header";
-import ItemListContainer from "./ItemList/ItemListContainer";
-import Counter from "./Counter/Counter"
+import Header from "Header/Header";
+import ItemListContainer from "ItemList/ItemListContainer";
+import Counter from "Counter/Counter"
 
 export {
     Header,
